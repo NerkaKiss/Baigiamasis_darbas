9e6c5a950a2a0ee1ef67d287ad6f31b35195a0bd
NerkaKiss <nerkai@gmail.com>
2024-12-16 21:12:18 +0200

    Implement TestListener of test failure

--- src/test/java/lt/vcs/pom/test/TestBase.java
+++ src/test/java/lt/vcs/pom/test/TestBase.java
@@ -1,15 +1,18 @@
 package lt.vcs.pom.test;
 
 import lt.vcs.pom.page.Common;
+import lt.vcs.pom.util.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public abstract class TestBase {
     @BeforeMethod
     public abstract void setUp();
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         Common.close();
     }
 }
